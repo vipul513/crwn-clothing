1dc62be93ec333fe69a5b3e65aa84a05d1171a80
Vipul <vipul@Vipuls-MacBook-Air.local>
2021-09-11 01:24:25 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router';
 
 import HomePage from './Pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>)
+}
 
 function App() {
     return ( 
       <div>
-        <HomePage/>
+        <Switch>
+          <Route exact path='/' component={HomePage}  />
+          <Route exact path='/shop/hats' component={HatsPage}  />
+         </Switch> 
       </div>
     );
 }
