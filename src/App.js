ece098099368fa564c66805b641139eb990843a0
Vipul <vipul@Vipuls-MacBook-Air.local>
2021-09-11 16:37:08 +0530

    Headers

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import { Route, Switch } from 'react-router';
 
 import HomePage from './Pages/homepage/homepage.component';
 import ShopPage from './Pages/shop/shop.component';
-
-const HatsPage = () => {
-  return (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>)
-}
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
     return ( 
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}  />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/shop/hats' component={HatsPage}  />
+          <Route exact path='/signin' component={SignInAndSignUpPage} />
          </Switch> 
       </div>
     );
